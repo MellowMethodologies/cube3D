8f3b254dd1236a3799dea19a3f81ff274686df54
Imad Dabligi <imad.dabligi@gmail.com>
2023-07-19 09:32:54 +0100

    IMAD Mini Map

--- ft_get_image.c
+++ ft_get_image.c
@@ -6,7 +6,7 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 15:33:04 by idabligi          #+#    #+#             */
-/*   Updated: 2023/07/15 10:10:30 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/07/19 08:09:02 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,17 @@ int	get_rgba(int r, int g, int b, int a)
 unsigned int	*ft_get_pixels(uint8_t *pxl, uint32_t count)
 {
 	unsigned int	*texture;
-	unsigned int	*tmp2;
-	uint8_t			*tmp;
+	unsigned int	*tmp;
 	int				i;
 
 	i = 0;
-	tmp = pxl;
 	texture = (unsigned int *)malloc(sizeof(unsigned int) * count);
 	if (!texture)
 		ft_abort(1);
-	tmp2 = texture;
+	tmp = texture;
 	while (i < (int)count)
 	{
-		tmp2[i++] = get_rgba(*pxl, *(pxl + 1), *(pxl + 2), *(pxl + 3));
+		tmp[i++] = get_rgba(*pxl, *(pxl + 1), *(pxl + 2), *(pxl + 3));
 		pxl += 4;
 	}
 	return (texture);
